53f90e9b90618f8e813e61ba431b15009d67dde2
Dmitry <60594670+YNDmitry@users.noreply.github.com>
2022-03-31 11:11:53 +0500

    change deploy settings

--- deploy.sh
+++ deploy.sh
@@ -13,6 +13,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:YNDmitry/ctech-table.git master:gh-pages
+git push -f git@github.com:doubleio/ctech-table.git master:gh-pages
 
 cd -
